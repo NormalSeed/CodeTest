54e1b404de967088a173e6b8bc27f39b045d23dc
NormalSeed <dlrjsghk11@naver.com>
2025-03-24 01:18:15 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.15 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        while ( answer * slice < n)
+        {
+            answer ++;
+        }
+        return answer;
+    }
+}
