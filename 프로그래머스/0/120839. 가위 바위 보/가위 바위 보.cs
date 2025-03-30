0958a1f374c7e572e969c4d408eff2cb4aba1642
NormalSeed <dlrjsghk11@naver.com>
2025-03-30 22:43:31 +0900

    Other Solution and Feedback
    람다식, Concat 사용법

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.cs
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.cs
@@ -2,31 +2,40 @@ using System;
 
 public class Solution {
     public string solution(string rsp)
-{
-    string[] rspStr = new string[rsp.Length];
-    for (int i = 0; i < rsp.Length; i++)
     {
-        rspStr[i] = rsp[i].ToString();
+        string[] rspStr = new string[rsp.Length];
+        for (int i = 0; i < rsp.Length; i++)
+        {
+            rspStr[i] = rsp[i].ToString();
+        }
+    
+        string[] result = new string[rspStr.Length];
+        for (int i = 0; i < rspStr.Length; i++)
+        {
+            if (rspStr[i] == "0")
+            {
+                result[i] = "5";
+            }
+            else if (rspStr[i] == "2")
+            {
+                result[i] = "0";
+            }
+            else if (rspStr[i] == "5")
+            {
+                result[i] = "2";
+            }
+        }
+    
+        string answer = String.Join("", result);
+        return answer;
     }
-
-    string[] result = new string[rspStr.Length];
-    for (int i = 0; i < rspStr.Length; i++)
-    {
-        if (rspStr[i] == "0")
-        {
-            result[i] = "5";
-        }
-        else if (rspStr[i] == "2")
-        {
-            result[i] = "0";
-        }
-        else if (rspStr[i] == "5")
-        {
-            result[i] = "2";
-        }
-    }
-
-    string answer = String.Join("", result);
-    return answer;
-}
+//    다른 사람 풀이 : 람다식과 Concat으로 구현, 람다식 연습하자
+//    public class Solution
+//    {
+//        public string solution(string rsp)
+//        {
+//            string answer = String.Concat(rsp.Select(x => x == '2' ? '0' : x == '0' ? '5' : '2'));
+//            return answer;
+//        }
+//    }
 }
