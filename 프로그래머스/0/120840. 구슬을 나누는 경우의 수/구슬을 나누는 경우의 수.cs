ae1e73cb7cb3804208b624e33c0aacd04cb634f5
NormalSeed <dlrjsghk11@naver.com>
2025-03-30 23:32:06 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.57 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cs
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cs
@@ -2,7 +2,8 @@ using System;
 
 public class Solution {
     public double solution(int balls, int share) {
-        // Combination  
+        // Factorial 계산 과정에서 큰 수를 다루게 되면 부동 소수점으로 처리됨.
+        // 그러므로 Math.Round 메서드로 반올림을 해주어 해결한다.
         double answer = Math.Round(Combination(balls, share));
         
         return answer;
