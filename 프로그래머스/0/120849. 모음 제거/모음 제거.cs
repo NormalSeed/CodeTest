3fed952719bd3962de74a775e0ab80c85e4ed586
NormalSeed <dlrjsghk11@naver.com>
2025-04-04 00:27:31 +0900

    Add Other's Solution
    모음 제거에 대한 다른 사람의 풀이를 추가

--- 프로그래머스/0/120849. 모음 제거/모음 제거.cs
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.cs
@@ -27,3 +27,21 @@ public class Solution {
         return answer;
     }
 }
+
+//다른사람 풀이
+//using System;
+
+//public class Solution
+//{
+//    public string solution(string my_string)
+//    {
+//        string answer = "";
+//        string[] b = new string[] { "a", "e", "i", "o", "u" };
+
+//        foreach (string c in b)
+//        {
+//            my_string = my_string.Replace(c, "");
+//        }
+//        return my_string;
+//    }
+//}
