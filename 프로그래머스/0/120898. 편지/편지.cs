f6871292ddb0670f3c4d330a4aacddfc9ee323ca
NormalSeed <dlrjsghk11@naver.com>
2025-04-12 10:38:11 +0900

    [level 0] Title: 편지, Time: 0.14 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public int solution(string message) {
+        int length = message.Length;
+        int answer = length * 2;
+        return answer;
+    }
+}
