0daea8b4a1f9124b30c375ea72fa01b69fb67340
NormalSeed <dlrjsghk11@naver.com>
2025-06-09 10:18:57 +0900

    [level 0] Title: 옹알이 (1), Time: 0.67 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Solution {
+    public int solution(string[] babbling) {
+        int answer = 0;
+        string str;
+        
+        for(int i = 0; i < babbling.Length; i++)
+        {
+            babbling[i] = babbling[i].Replace("aya", "x");
+            babbling[i] = babbling[i].Replace("ye", "x");
+            babbling[i] = babbling[i].Replace("woo", "x");
+            babbling[i] = babbling[i].Replace("ma", "x");
+            
+            str = babbling[i].Replace("x", "");
+            
+            if (str == "")
+            {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
